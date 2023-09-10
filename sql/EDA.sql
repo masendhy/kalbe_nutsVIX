186c8187ae1b494b918f18a5a4cee5902a2ffc79
sendhy boedhi <masendhy@sendhys-MacBook-Pro.local>
2023-09-10 13:57:20 +0700

    EDA done

--- sql/EDA.sql
+++ sql/EDA.sql
@@ -1,5 +1,5 @@
 
--- query 􏰁 : Berapa rata-rata umur customer jika dilihat dari marital statusnya ?
+-- query 1 : Berapa rata-rata umur customer jika dilihat dari marital statusnya ?
 SELECT 
 CASE 
     WHEN "Marital Status" ='' THEN 'Unknown'  
@@ -10,5 +10,41 @@ FROM customer
 GROUP BY marital_status
 ORDER BY average_age DESC;
 
---query 􏰂 : Berapa rata-rata umur customer jika dilihat dari gender nya ?
+--query 2 : Berapa rata-rata umur customer jika dilihat dari gender nya ?
 
+SELECT
+CASE 
+    WHEN Gender = 0 THEN 'female'
+    WHEN Gender = 1 THEN 'male'
+END as gender_label,
+round(Avg(age),2) as average_age
+FROM customer
+GROUP BY gender_label
+ORDER BY average_age DESC;
+
+--query 3 : Tentukan nama store dengan total quantity terbanyak!
+
+with table_1 as (
+    SELECT
+    store.storename,
+    transaction.qty
+    from store JOIN transaction on transaction.storeid = store.storeid
+)
+SELECT storename, sum(qty) as total_qty
+FROM table_1
+GROUP BY storename
+ORDER BY total_qty DESC;
+
+--query 4 : Tentukan nama produk terlaris dengan total amount terbanyak!
+
+with table_1 as (
+    SELECT
+    product."Product Name",
+    transaction.totalamount
+    from product JOIN transaction on transaction.productid = product.productid
+)
+SELECT "Product Name", 
+sum(totalamount) as total_amount
+FROM table_1
+GROUP BY "Product Name"
+ORDER BY total_amount DESC;
